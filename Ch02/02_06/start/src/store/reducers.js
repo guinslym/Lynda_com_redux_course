6aaf1e2b39812bfaa9fc41f77786b937dbeed29c
Guinsly Mondésir <guinslym@gmail.com>
2017-09-17 21:03:06 -0400

    challenges

--- Ch02/02_06/start/src/store/reducers.js
+++ Ch02/02_06/start/src/store/reducers.js
@@ -1,11 +1,11 @@
 import C from '../constants'
 
-export const goal = (state=10, action) => 
-	(action.type === C.SET_GOAL) ? 
+export const goal = (state=10, action) =>
+	(action.type === C.SET_GOAL) ?
 		 parseInt(action.payload) :
 		 state
 
-export const skiDay = (state=null, action) => 
+export const skiDay = (state=null, action) =>
   (action.type === C.ADD_DAY) ?
   	action.payload :
   	state
@@ -17,18 +17,54 @@ export const errors = (state=[], action) => {
          ...state,
          action.payload
     	]
-    case C.CLEAR_ERROR : 
+    case C.CLEAR_ERROR :
       return state.filter((message, i) => i !== action.payload)
-  	default: 
+  	default:
   		return state
   }
 }
+
+
+
+export const suggestions = (state=[], action) =>{
+	switch (action.type) {
+		case C.CLEAR_SUGGESTIONS:
+			return []
+
+		case C.CHANGE_SUGGESTIONS:
+			return action.payload
+
+		default:
+			return state
+
+	}
+}
+
+
+export const fetching = (state=false, action) =>{
+	switch (action.type) {
+		case C.FETCH_RESORT_NAMES:
+			return true
+
+		case C.CANCEL_FETCHING:
+			return false
+
+		case C.CHANGE_SUGGESTIONS:
+			return false 
+
+		default:
+			return state
+
+	}
+}
+
+
 
 export const allSkiDays = (state=[], action) => {
 
   switch(action.type) {
 
-    case C.ADD_DAY : 
+    case C.ADD_DAY :
 
       const hasDay = state.some(skiDay => skiDay.date === action.payload.date)
 
@@ -41,7 +77,7 @@ export const allSkiDays = (state=[], action) => {
 
     case C.REMOVE_DAY :
 
-      return state.filter(skiDay => skiDay.date !== action.payload)     
+      return state.filter(skiDay => skiDay.date !== action.payload)
 
     default:
       return state
